956c19a8e55a8187afa3a5027f1e5ddea18631a6
Connor Worrell <chemicalcoal1@gmail.com>
2022-01-29 11:31:42 -0700

    Font size increased, dmenu runs case insentitive, mod+c calculator shortcut

--- config.def.h
+++ config.def.h
@@ -12,8 +12,8 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=9"};
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=14"};
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -107,13 +107,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *randWall[] =  { "setWallpaper", "-r", NULL };
 // static const char *termcmd[]  = { "st", NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 static const char *lock[] = { "lock", NULL };
 static const char *volume[] = { "urxvt", "-e", "pulsemixer" , NULL };
 static const char *browser[] = { "firejail", "brave" , NULL };
+static const char *calculator[] = {"urxvt", "-e", "qalc", NULL };
 
 
 static Key keys[] = {
@@ -124,6 +125,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_w,      spawn,          {.v = randWall} },
 	{ MODKEY,                       XK_v,      spawn,          {.v = volume} },
 	{ MODKEY,                       XK_n,      spawn,          {.v = browser} },
+	{ MODKEY,                       XK_c,      spawn,          {.v = calculator}},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	STACKKEYS(MODKEY,                          focus)
 	STACKKEYS(MODKEY|ShiftMask,                push)
