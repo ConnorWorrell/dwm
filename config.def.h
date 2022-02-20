ff34202c92a6d8be4b108531bbf3e10361e4a2e8
Connor Worrell <chemicalcoal1@gmail.com>
2022-02-19 22:02:10 -0700

    switched terminal emulator to st

--- config.def.h
+++ config.def.h
@@ -106,7 +106,7 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-static const char terminalemulator[] = "alacritty";
+static const char terminalemulator[] = "st";
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
