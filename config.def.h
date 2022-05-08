c3349f7311b783131e137562719ffcbe9100f544
Connor Worrell <chemicalcoal1@gmail.com>
2022-05-07 19:05:19 -0600

    dmenu fuzzy finder highlighting colors

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -18,8 +18,8 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 180;
+static const char col_cyan[]        = "#004d4d";
+static const unsigned int baralpha = 110;
 static const unsigned int borderalpha = OPAQUE;
 static const char col1[]            = "#ffffff";
 static const char col2[]            = "#ffffff";
@@ -138,7 +138,7 @@ static const char terminalemulator[] = "st";
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nhb", col_gray1,"-nf", col_gray3, "-sb", col_cyan, "-shb", col_cyan,"-shb", col_cyan,"-sf", col_gray4, NULL };
 static const char *randWall[] =  { "setWallpaper", "-r", NULL };
 // static const char *termcmd[]  = { "st", NULL };
 static const char *termcmd[]  = { "bash", "-c", "adjustTerminal || st",  NULL };
