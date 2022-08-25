95677c8465d4ed6fda1c5a96be2383c4279b5445
Connor Worrell <chemicalcoal1@gmail.com>
2022-08-24 21:02:45 -0600

    Removed bar

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=14"};
 static const char dmenufont[]       = "monospace:size=14";
